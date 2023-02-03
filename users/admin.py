9d4295a2703cf0360c09eac2e25d28c12780adc6
hagunhee <hatchi10@gmail.com>
2023-02-04 04:16:56 +0900

    air 5.3

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": ("username", "password", "name", "email", "is_host"),
+            },
+        ),
+    )
